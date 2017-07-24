a3f1c72c1218ce6d54d4d5fc3c2f284b08caa394
araicu <a.raicu@sap.com>
2017-07-24 11:22:35 +0300

    test server

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,2 +1,23 @@
 /* Hello World! program in Node.js */
-console.log("Hello World!!!");
+"use strict";
+
+//required module
+var mHttp = require("http");
+
+//variables
+var iPort = 8081;
+var sMessage = "Message";
+//create Http Server
+var oServer = mHttp.createServer(function(req, res){
+    console.log("Request Received");
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(sMessage);
+    res.end();
+
+    console.log("Response Send");
+} );
+oServer.listen(iPort)
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:' + iPort + '/');
