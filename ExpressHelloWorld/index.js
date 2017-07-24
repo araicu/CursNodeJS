b0ba2378221dc49748db1a3ca47e5c46e774684c
araicu <a.raicu@sap.com>
2017-07-24 12:16:59 +0300

    no message

--- ExpressHelloWorld/index.js
+++ ExpressHelloWorld/index.js
@@ -3,10 +3,14 @@
 var express = require('express');
 var app = express();
  
-app.get('/', function (req, res) {
+app.get('/Hello/', function (req, res) {
   res.send('Hello World !!');
 });
  
+app.post('/', function (req, res) {
+  res.send('No Post methoo available');
+});
+
 app.listen(3000);
 
 console.log('Server running at http://127.0.0.1:' + 3000 + '/');
