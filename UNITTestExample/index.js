04899e10a3b27f43ebaf4cdcc5977dd6dca4bf08
araicu <a.raicu@sap.com>
2017-07-25 12:28:49 +0300

    commit cu erori test fara im[plementare

--- UNITTestExample/index.js
+++ UNITTestExample/index.js
@@ -2,7 +2,7 @@ console.log("unit starts");
 
 var myValidator = require("./modules/passwordValidator.js");
 
-var password = "testBauBau2018";
+var password = "test";
 var valid = myValidator.validPassword(password);
 
 console.log("Your password ' " + password + "' is : " + valid);
