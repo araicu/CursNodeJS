503ce15e0e5e45e9f3cebd3c6c8e46e00a9ac56b
araicu <a.raicu@sap.com>
2017-07-25 12:31:23 +0300

    no message

--- UNITTestExample/modules/passwordValidator.js
+++ UNITTestExample/modules/passwordValidator.js
@@ -1,3 +1,10 @@
 exports.check = module.exports.validPassword = function(password){
+    if (password.lenght < 8){
+        return false;
+    }
+    if(password.indexOf('password') >= 0){
+        return false;
+    }
+
     return true;
 };
